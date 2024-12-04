0c44b6d02f8b906c9a1a557103073cbfdcaffc00
Ferdous Ahmed Himel <himibaba10@gmail.com>
2024-12-05 00:39:33 +0600

    Created the title, description, and button for hero section

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -19,7 +19,7 @@ const Header = () => {
         {/* Nav Links */}
         <div
           className={`w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-s2 max-lg:opacity-${
-            isOpen ? "100" : "0"
+            isOpen ? "100" : "0 pointer-events-none"
           }`}
         >
           <div className="max-lg:relative max-lg:flex max-lg:flex-col max-lg:min-h-screen max-lg:p-6 max-lg:overflow-hidden sidebar-before max-md:px-4">
