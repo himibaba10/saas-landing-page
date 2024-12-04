be31d7184f88c9c4d9d18b5af9f10f84b0e47232
Ferdous Ahmed Himel <himibaba10@gmail.com>
2024-12-05 00:48:37 +0600

    Created the hero section completely

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -5,6 +5,7 @@ const Hero = () => {
   return (
     <section className="relative pt-60 pb-40 max-lg:pt-52 max-lg:pb-36 max-md:pt-36 max-md:pb-32">
       <Element name="hero">
+        {/* Left Side */}
         <div className="container">
           <div className="relative z-2 max-w-512 max-lg:max-w-388">
             <div className="caption small-2 uppercase text-p3">
@@ -23,6 +24,15 @@ const Hero = () => {
               </Button>
             </LinkScroll>
           </div>
+        </div>
+
+        {/* Image */}
+        <div className="absolute -top-32 left-[calc(50%-340px)] w-1230 pointer-events-none hero-img_res">
+          <img
+            src="/images/hero.png"
+            className="size-1230 max-lg:h-auto"
+            alt="hero"
+          />
         </div>
       </Element>
     </section>
