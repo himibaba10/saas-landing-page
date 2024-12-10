83df2148a2f2808b89d115274864d0b945df0fb9
Ferdous Ahmed Himel <himibaba10@gmail.com>
2024-12-10 21:04:00 +0600

    The header part is completely done

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,16 +1,42 @@
-import { useState } from "react";
+import clsx from "clsx";
+import { useEffect, useState } from "react";
 import { Link as LinkScroll } from "react-scroll";
 
-const NavLink = ({ title }) => (
-  <LinkScroll className="base-bold text-p4 uppercase transition-colors duration-500 cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h5">
-    {title}
-  </LinkScroll>
-);
-
 const Header = () => {
+  const [hasScrolled, setHasScrolled] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setHasScrolled(window.scrollY > 32);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const NavLink = ({ title }) => (
+    <LinkScroll
+      onClick={() => setIsOpen(false)}
+      to={title}
+      offset={-100}
+      spy
+      smooth
+      activeClass="nav-active"
+      className="base-bold text-p4 uppercase transition-colors duration-500 cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h5"
+    >
+      {title}
+    </LinkScroll>
+  );
+
   return (
-    <header className="fixed top-0 left-0 z-50 w-full py-10">
+    <header
+      className={clsx(
+        "fixed top-0 left-0 z-50 w-full py-10 max-lg:py-4 transition-all duration-300",
+        hasScrolled && "py-2 bg-black-100"
+      )}
+    >
       <div className="container flex h-14 items-center max-lg:px-5">
         <a className="lg:hidden flex-1 cursor-pointer z-2" href="#">
           <img src="/images/xora.svg" width={115} height={55} alt="logo" />
@@ -19,7 +45,7 @@ const Header = () => {
         {/* Nav Links */}
         <div
           className={`w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-s2 max-lg:opacity-${
-            isOpen ? "100" : "0 pointer-events-none"
+            isOpen ? "100" : "0 max-lg:pointer-events-none"
           }`}
         >
           <div className="max-lg:relative max-lg:flex max-lg:flex-col max-lg:min-h-screen max-lg:p-6 max-lg:overflow-hidden sidebar-before max-md:px-4">
@@ -44,6 +70,7 @@ const Header = () => {
                       width={160}
                       height={55}
                       alt="logo"
+                      className="cursor-pointer"
                     />
                   </LinkScroll>
                 </li>
