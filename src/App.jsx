0e1ffb53eea789365706d9cac8aa655422ff0bcb
Ferdous Ahmed Himel <himibaba10@gmail.com>
2024-12-18 18:30:24 +0600

    created the testimonials section

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,17 @@ import Features from "./sections/Features";
 import Header from "./sections/Header";
 import Hero from "./sections/Hero";
 import Pricing from "./sections/Pricing";
+import Testimonials from "./sections/Testimonials";
 
 const App = () => {
   return (
-    <main className="overflow-hidden">
+    <main className="overflow-clip">
       <Header />
       <Hero />
       <Features />
       <Pricing />
       <Faq />
+      <Testimonials />
     </main>
   );
 };
---
+++ src/components/TestimonialItem.jsx
@@ -0,0 +1,32 @@
+import clsx from "clsx";
+
+const TestimonialItem = ({
+  item: { comment, avatarUrl, name, role },
+  containerClassName,
+}) => {
+  return (
+    <div
+      className={clsx(
+        "relative px-14 pb-14 pt-11 after:absolute after:bottom-0 after:right-0 after:h-0.5 after:w-screen after:bg-s2 after:content-[''] max-md:px-0 max-md:pt-11 after:max-md:-right-4",
+        containerClassName
+      )}
+    >
+      <blockquote className="h6 mb-8 text-p4">{comment}</blockquote>
+
+      <div className="flex items-center max-xl:-mr-8">
+        {/* Avatar Image */}
+        <div className="mr-4 size-20 shrink-0 rounded-half border-2 border-s2 p-1.5">
+          <img src={avatarUrl} alt={name} className="size-full object-cover" />
+        </div>
+
+        {/* User name & role */}
+        <div>
+          <h4 className="body-2 mb-0.5 text-p1">{name}</h4>
+          <p className="small-compact uppercase text-s3">{role}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TestimonialItem;
